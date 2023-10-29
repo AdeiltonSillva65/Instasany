9c6d1eafb9e98b985b1f37a51a055104f18b31ae
Adeilton Francisco da Silva <adeiltonsillva65@gmail.com>
2023-10-29 18:31:31 -0300

    animação do celular principal

--- instasany/src/components/section-cards.tsx
+++ instasany/src/components/section-cards.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 
 import Mockup from '@/../../public/mockup-main.png'
@@ -7,15 +8,34 @@ import Card03 from '@/../../public/image-03.png'
 import Card04 from '@/../../public/image-04.png'
 import Card05 from '@/../../public/image-05.png'
 import Card06 from '@/../../public/image-06.png'
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const  SectionCards = ()=>{
+
+    const mockupRef = useRef(null)
+
+    useEffect(()=>{
+        const mockupPhone = mockupRef.current;
+
+        gsap.fromTo(mockupPhone, {
+            opacity:0,
+            scale: .5
+        },{
+            opacity:1,
+            scale:1,
+            duration: 1,
+            ease: 'power5.out'
+        })
+    },[]);
+
     return(
         <>
             <section className="w-full pb-20">
                 <Image
                     src={Mockup}
                     alt="mokup main"
-                    className=" sticky mx-auto top-56 z-10 -mt-[32rem] mb-16px"
+                    className=" sticky mx-auto top-56 z-10 -mt-[32rem] mb-16px opacity-0" ref={mockupRef}
                 />
 
                 <h2 className="text-center text-black font-semibold text-7xl mb-56">Faça <span className="text-green-title-card">você</span> mesmo de casa</h2>
